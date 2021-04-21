9bbdf24fbafd792d7524b66ed593512b009b0f8f
rachid <olgor@hotmail.com>
2021-04-21 18:53:00 +0200

    connect to database added

---
+++ server/src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    database: 'dvdDataBase',
+    username: 'root',
+    password: 'root',
+    host: 'localhost'
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,16 @@
 const app = require('./app');
+const sequelize = require('./database/db');
 
 // starting the server
 
-async function main(){
+function startConnection(){
+    sequelize.authenticate().then(()=>{
+        console.log("Database connected")
+    });
+}
 
+async function main(){
+    startConnection();
     app.listen(app.get('port'));
     console.log('Server is running on port', app.get('port'));
 }

4a3eda2ccffe16404b34258d1699d7932638f0b5
rachid <olgor@hotmail.com>
2021-04-22 07:02:19 +0200

    user model added

---
+++ server/src/models/user.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+	const User = sequelize.define('users', {
+		// userId :{
+		// 	type:Sequelize.INTEGER
+		// },
+	  username: {
+		  type: Sequelize.STRING
+	  },
+	  email: {
+		  type: Sequelize.STRING
+	  },
+	  password: {
+		  type: Sequelize.STRING
+	  },
+      role: {
+        type: Sequelize.STRING
+    }
+	  
+	},{
+		timestamps: false
+	});
+	
+	return User;
+}

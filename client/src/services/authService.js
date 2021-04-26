9d4058571089bd056dd5fe140b4807e5117409da
rachid <olgor@hotmail.com>
2021-04-26 16:58:32 +0200

    loggedin and logout added to authservice

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -5,8 +5,8 @@ const URL = 'http://localhost:5050/api'
 class AuthService {
   async login(user) {
     console.log(user)
-    const res = await axios.post(URL+'/signin', user, {
-      
+    const res = await axios.post(URL + '/signin', user, {
+
     });
 
     return res;
@@ -14,11 +14,33 @@ class AuthService {
 
   async signup(user) {
     console.log(user)
-    const res = await axios.post(URL+'/signup', user, {
-      
+    const res = await axios.post(URL + '/signup', user, {
+
     });
 
     return res;
+  }
+
+  //get usr from localhost
+  getUser() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user != "undefined") {
+      this.user = user
+    } else {
+      this.user = null;
+    }
+    return this.user;
+  }
+
+  //If is logged in
+  loggedIn(){
+    return !!localStorage.getItem('token') && !!localStorage.getItem('user');
+  }
+  //To Log Out
+  logOut(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.router.navigate(['/login'])
   }
 
 }
